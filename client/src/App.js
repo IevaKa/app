85413037c6fa3a2164eb076a4f3c694e682ba5a7
Eduarda Liu <eduardaliu@gmail.com>
2020-07-22 10:03:06 +0200

    components structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,54 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import './App.css';
-import { Route } from 'react-router-dom';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.css";
+
+import Home from "./components/Home";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import MainDash from "./components/MainDash";
+
+
+// import "./App.css";
 // import Projects from './components/Projects';
-import Navbar from './components/Navbar';
-import Tickets from './components/Tickets';
-import AddTicket from './components/AddTicket';
+// import Navbar from "./components/Navbar";
+// import Tickets from "./components/Tickets";
+// import AddTicket from "./components/AddTicket";
 // import ProjectDetails from './components/ProjectDetails';
 // import TaskDetails from './components/TaskDetails';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
+class App extends Component {
+  state = {
+    user: this.props.user,
+  };
+
+  setUser = (user) => {
+    this.setState({
+      user: user,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <Route exact path="/" component={Home} />
+        <Route
+          exact
+          path="/signup"
+          render={(props) => <Signup setUser={this.setUser} {...props} />}
+        />
+        <Route
+          exact
+          path="/login"
+          render={(props) => <Login setUser={this.setUser} {...props} />}
+        />
+        <Route
+          exact
+          path="/dash"
+          render={(props) => <MainDash user={this.user} test={'test'} setUser={this.setUser} {...props} />}
+        />
+
+        {/* <Navbar />
 
       <Route
         exact
@@ -24,9 +60,10 @@ function App() {
         exact
         path='/tickets/create'
         component={AddTicket}
-      />
-    </div>
-  );
+      /> */}
+      </>
+    );
+  }
 }
 
 export default App;
