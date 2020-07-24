c86da1a0c0ad443074c27bc710e445dd6a5396a7
Ieva Kaziukonyte <kaziukonyte.ieva@gmail.com>
2020-07-24 14:31:27 +0200

    pull from ivan

--- routes/column.js
+++ routes/column.js
@@ -16,4 +16,18 @@ router.get('/', (req, res) => {
     });
 });
 
+router.post('/', (req, res) => {
+  Column.findOne({ user: req.user.id })
+  // .populate('columnOpen')
+  // .populate('columnProgress')
+  // .populate('columnDone')
+  // .populate('columnCancelled')
+  .then(column => {
+      res.status(200).json(column);
+    })
+    .catch(err => {
+      res.json(err);0
+    });
+});
+
 module.exports = router;
