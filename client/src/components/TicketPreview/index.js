c8532860836e23a70feb334ca33a8f3e24cc6e51
Eduarda Liu <eduardaliu@gmail.com>
2020-07-28 09:44:48 +0200

    added TA status to ticket preview

--- client/src/components/TicketPreview/index.js
+++ client/src/components/TicketPreview/index.js
@@ -2,7 +2,14 @@ import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { Draggable } from "react-beautiful-dnd";
-import { ironBlue, lightBlue, lightGray } from "../../styles/global.js";
+import {
+  ironBlue,
+  ironPurple,
+  ironRed,
+
+  lightBlue,
+  lightGray,
+} from "../../styles/global.js";
 
 import Moment from "react-moment";
 
@@ -12,13 +19,14 @@ const Container = styled.div`
   font-size: 14px;
   border: 1px solid ${lightGray};
   width: 210px;
-  padding: 10px;
+  padding: 12px 10px 18px 10px;
   margin-bottom: 8px;
   border-radius: 5px;
   box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.05);
   color: ${(props) => (props.isDragging ? "white" : "black")};
   background-color: ${(props) => (props.isDragging ? ironBlue : "white")};
   transition: all 300ms ease-in-out;
+  ${"" /* background-color: red */}
 `;
 
 const LabTag = styled.div`
@@ -42,19 +50,32 @@ const TicketOwner = styled.img`
   border-radius: 100%;
   width: 20px;
   height: 20px;
-
   display: inline-block;
+`;
+
+const TAstatus = styled.div`
+  color: white;
+  text-align: right;
+  position: absolute;
+  font-size: 11px;
+  padding: 2px 5px 2px 5px;
+  border-radius: 100px;
+  margin: 8px 0 0 120px;
+  background-color: ${ironRed};
 `;
 
 export default class TicketPreview extends React.Component {
   render() {
     const timestamp = this.props.ticket.createdAt;
 
-    // console.log(this.props.ticket.createdBy);
+    console.log(this.props.ticket);
     // console.log(this.props.allUsers);
 
     let checkUser = (user) => user._id === this.props.ticket.createdBy;
     const ticketOwner = this.props.allUsers.find(checkUser);
+
+    let checkTA = (user) => user._id === this.props.ticket.assignee;
+    const ticketTA = this.props.allUsers.find(checkTA);
 
     // console.log("OWNER IS" + ticketOwner);
     return (
@@ -82,6 +103,8 @@ export default class TicketPreview extends React.Component {
               <Timestamp isDragging={snapshot.isDragging}>
                 Created <Moment fromNow>{timestamp}</Moment>
               </Timestamp>
+
+              {ticketTA && <TAstatus>{ticketTA.name} is on it!</TAstatus>}
             </Container>
           </Link>
         )}
